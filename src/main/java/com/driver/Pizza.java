c46ba68a2807266cedb590f1f899ac57b40b63f5
Shoaib Akhtar <sk370008@gmail.com>
2023-02-11 12:07:22 +0530

    First Attempt

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,21 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+
+    boolean isExtraCheese;
+    boolean isExtraToppings;
+    boolean forTakeaway;
+    boolean isBillGenerated;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price += 300;
+        }else {
+            this.price += 400;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +29,53 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheese){
+            this.price += 80;
+            isExtraCheese = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!isExtraToppings){
+            if(isVeg){
+                this.price += 70;
+            }else {
+                this.price += 120;
+            }
+            isExtraToppings = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!forTakeaway){
+            price += 20;
+            forTakeaway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(isVeg){
+            System.out.println("Base Price Of The Pizza: 300");
+            if(isExtraCheese) System.out.println("Extra Cheese Added: 80");
+            if (isExtraToppings) System.out.println("Extra Toppings Added: 70");
+            if (forTakeaway) System.out.println("Paperbag Added: 20");
+            if (!isBillGenerated){
+                isBillGenerated = true;
+                return "Total Price: " + String.valueOf(getPrice());
+            }
+        }else{
+            System.out.println("Base Price Of The Pizza: 400");
+            if(isExtraCheese) System.out.println("Extra Cheese Added: 80");
+            if (isExtraToppings) System.out.println("Extra Toppings Added: 120");
+            if (forTakeaway) System.out.println("Paperbag Added: 20");
+            if (!isBillGenerated){
+                isBillGenerated = true;
+                return "Total Price: " + String.valueOf(getPrice());
+            }
+        }
+        return "";
     }
 }
